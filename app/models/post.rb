c80accc79684e7a6b6a0b03981fbc0ebaaf0af93
Justiceshrs C. Portal <justiceshrsportal@gmail.com>
2020-12-07 23:44:15 +0800

    fix post deletion

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :reactions, dependent: :destroy
-  has_many :comments
+  has_many :comments, dependent: :destroy
   
   mount_uploader :photo, PhotoUploader
 
